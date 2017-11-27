2e966d72ab658dcc21be0f5010aaaf00b66d8ee7
Tony Reina <greina@eng.ucsd.edu>
2017-11-27 15:52:58 -0800

    Trace works now

--- train.py
+++ train.py
@@ -18,10 +18,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--use_upsampling', help='use upsampling instead of transposed convolution',
 					action='store_true', default=False)
-parser.add_argument("--num_threads", type=int, default=34, help="the number of threads")
+parser.add_argument("--num_threads", type=int, default=50, help="the number of threads")
 parser.add_argument("--num_inter_threads", type=int, default=2, help="the number of intraop threads")
-parser.add_argument("--batch_size", type=int, default=256, help="the batch size for training")
+parser.add_argument("--batch_size", type=int, default=2128, help="the batch size for training")
 parser.add_argument("--blocktime", type=int, default=0, help="blocktime")
+parser.add_argument("--epochs", type=int, default=10, help="number of epochs to train")
+parser.add_argument("--learningrate", type=float, default=0.0001, help="learningrate")
 
 args = parser.parse_args()
 
@@ -37,6 +39,10 @@ if (args.blocktime > 1000):
 else:
 	blocktime = str(args.blocktime)
 
+EPOCHS = args.epochs
+learningrate = args.learningrate
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'  # Get rid of the AVX, SSE warnings
 
 os.environ["KMP_BLOCKTIME"] = blocktime
@@ -47,7 +53,7 @@ os.environ["KMP_AFFINITY"]="compact,1,0,granularity=thread"
 os.environ["OMP_NUM_THREADS"]= str(num_threads)
 os.environ["TF_ADJUST_HUE_FUSED"] = '1'
 os.environ['TF_ADJUST_SATURATION_FUSED'] = '1'
-#os.environ['MKL_VERBOSE'] = '1'
+os.environ['MKL_VERBOSE'] = '1'
 os.environ['MKL_DYNAMIC']='1'
 os.environ['INTRA_THREADS']=str(num_threads)
 os.environ['INTER_THREADS']=str(num_inter_op_threads)
@@ -93,7 +99,8 @@ run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 run_metadata = tf.RunMetadata()  # For Tensorflow trace
 
 from keras import backend as K
-K.set_image_dim_ordering('tf')	
+K.set_session(sess)
+
 from keras.callbacks import ModelCheckpoint, TensorBoard, LearningRateScheduler
 from keras.callbacks import History 
 from keras.models import Model
@@ -106,6 +113,120 @@ from helper import *
 import settings
 
 from keras.preprocessing.image import ImageDataGenerator
+
+def model5_MultiLayer(args=None, weights=False, 
+	filepath="", 
+	img_rows = 224, 
+	img_cols = 224, 
+	n_cl_in=3,
+	n_cl_out=3, 
+	dropout=0.2, 
+	learning_rate = 0.01,
+	print_summary = False):
+	""" difference from model: img_rows and cols, order of axis, and concat_axis"""
+	
+	if args.use_upsampling:
+		print ('Using UpSampling2D')
+	else:
+		print('Using Transposed Deconvolution')
+
+	inputs = Input((img_rows, img_cols, n_cl_in), name='Image')
+
+	params = dict(kernel_size=(3, 3), activation='relu', 
+				  padding='same', 
+				  kernel_initializer='he_uniform') #RandomUniform(minval=-0.01, maxval=0.01, seed=816))
+
+	conv1 = Conv2D(name='conv1a', filters=32, **params)(inputs)
+	conv1 = Conv2D(name='conv1b', filters=32, **params)(conv1)
+	pool1 = MaxPooling2D(name='pool1', pool_size=(2, 2))(conv1)
+
+	conv2 = Conv2D(name='conv2a', filters=64, **params)(pool1)
+	conv2 = Conv2D(name='conv2b', filters=64, **params)(conv2)
+	pool2 = MaxPooling2D(name='pool2', pool_size=(2, 2))(conv2)
+
+	conv3 = Conv2D(name='conv3a', filters=128, **params)(pool2)
+	conv3 = Dropout(dropout)(conv3) ### Trying dropout layers earlier on, as indicated in the paper
+	conv3 = Conv2D(name='conv3b', filters=128, **params)(conv3)
+	
+	pool3 = MaxPooling2D(name='pool3', pool_size=(2, 2))(conv3)
+
+	conv4 = Conv2D(name='conv4a', filters=256, **params)(pool3)
+	conv4 = Dropout(dropout)(conv4) ### Trying dropout layers earlier on, as indicated in the paper
+	conv4 = Conv2D(name='conv4b', filters=256, **params)(conv4)
+	
+	pool4 = MaxPooling2D(name='pool4', pool_size=(2, 2))(conv4)
+
+	conv5 = Conv2D(name='conv5a', filters=512, **params)(pool4)
+	
+
+	if args.use_upsampling:
+		conv5 = Conv2D(name='conv5b', filters=256, **params)(conv5)
+		up6 = concatenate([UpSampling2D(name='up6', size=(2, 2))(conv5), conv4], axis=-1)
+	else:
+		conv5 = Conv2D(name='conv5b', filters=512, **params)(conv5)
+		up6 = concatenate([Conv2DTranspose(name='transConv6', filters=256, 
+			               kernel_size=(2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=-1)
+		
+	conv6 = Conv2D(name='conv6a', filters=256, **params)(up6)
+	
+
+	if args.use_upsampling:
+		conv6 = Conv2D(name='conv6b', filters=128, **params)(conv6)
+		up7 = concatenate([UpSampling2D(name='up7', size=(2, 2))(conv6), conv3], axis=-1)
+	else:
+		conv6 = Conv2D(name='conv6b', filters=256, **params)(conv6)
+		up7 = concatenate([Conv2DTranspose(name='transConv7', filters=128, 
+			               kernel_size=(2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=-1)
+
+	conv7 = Conv2D(name='conv7a', filters=128, **params)(up7)
+	
+
+	if args.use_upsampling:
+		conv7 = Conv2D(name='conv7b', filters=64, **params)(conv7)
+		up8 = concatenate([UpSampling2D(name='up8', size=(2, 2))(conv7), conv2], axis=-1)
+	else:
+		conv7 = Conv2D(name='conv7b', filters=128, **params)(conv7)
+		up8 = concatenate([Conv2DTranspose(name='transConv8', filters=64, 
+			               kernel_size=(2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=-1)
+
+	
+	conv8 = Conv2D(name='conv8a', filters=64, **params)(up8)
+	
+	if args.use_upsampling:
+		conv8 = Conv2D(name='conv8b', filters=32, **params)(conv8)
+		up9 = concatenate([UpSampling2D(name='up9', size=(2, 2))(conv8), conv1], axis=-1)
+	else:
+		conv8 = Conv2D(name='conv8b', filters=64, **params)(conv8)
+		up9 = concatenate([Conv2DTranspose(name='transConv9', filters=32, 
+			               kernel_size=(2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=-1)
+
+
+	conv9 = Conv2D(name='conv9a', filters=32, **params)(up9)
+	conv9 = Conv2D(name='conv9b', filters=32, **params)(conv9)
+
+	conv10 = Conv2D(name='Mask', filters=n_cl_out, kernel_size=(1, 1), activation='sigmoid')(conv9)
+
+	model = Model(inputs=[inputs], outputs=[conv10])
+
+	# if weights:
+	# 	optimizer=Adam(lr=0.0001, beta_1=0.9, beta_2=0.99, epsilon=1e-08, decay=0.01)
+	# else:
+	# 	optimizer = SGD(lr=learning_rate, momentum=0.9, decay=0.05)
+
+	optimizer=Adam(lr=args.learningrate, beta_1=0.9, beta_2=0.99, epsilon=1e-08, decay=0.00001)
+
+	model.compile(optimizer=optimizer,
+		loss=dice_coef_loss, #dice_coef_loss, #'binary_crossentropy', 
+		metrics=['accuracy', dice_coef], options=run_options, run_metadata=run_metadata)
+
+	if weights and os.path.isfile(filepath):
+		print('Loading model weights from file {}'.format(filepath))
+		model.load_weights(filepath)
+
+	if print_summary:
+		print (model.summary())	
+
+	return model
 
 def image_augmentation(imgs, masks): 
 	'''
@@ -281,7 +402,7 @@ if __name__ =="__main__":
 
 	train_and_predict(settings.OUT_PATH, settings.IMG_ROWS/settings.RESCALE_FACTOR, 
 		settings.IMG_COLS/settings.RESCALE_FACTOR, 
-		settings.EPOCHS, settings.IN_CHANNEL_NO, \
+		EPOCHS, settings.IN_CHANNEL_NO, \
 		settings.OUT_CHANNEL_NO, settings.MODEL_FN, settings.MODE, args)
 
 	print('Total time elapsed for program = {} seconds'.format(time.time() - start_time))
