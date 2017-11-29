5d610437ee76fb67a13df0ffb407c50de6620c5e
Tony Reina <greina@eng.ucsd.edu>
2017-11-29 14:57:18 -0800

    Better Dice loss for underflow

--- helper.py
+++ helper.py
@@ -54,6 +54,12 @@ def dice_coef(y_true, y_pred, smooth = 1. ):
 	return coef
 
 def dice_coef_loss(y_true, y_pred):
-	return -K.log(dice_coef(y_true, y_pred))
+
+	smooth = 1.
+	y_true_f = K.flatten(y_true)
+	y_pred_f = K.flatten(y_pred)
+	intersection = K.sum(y_true_f * y_pred_f)
+	loss = -K.log(2. * intersection + smooth) + K.log((K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+	return loss
 
 
