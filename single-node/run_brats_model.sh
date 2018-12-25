87e842540b2b2c09f6634d44c31ec1b542e8aa4e
Tony.Reina <greina@eng.ucsd.edu>
2018-12-25 11:35:45 -0800

    Updating bash script to check for data directory

--- single-node/run_brats_model.sh
+++ single-node/run_brats_model.sh
@@ -46,15 +46,6 @@ MODEL_OUTPUT_FILENAME=${5:-"decathlon_brats.h5"}
 INFERENCE_FILENAME=${6:-"unet_model_for_inference.hdf5"}
 
 
-if [[ ! -f ${DECATHLON_DIR}/dataset.json ]] ; then
-    clear
-    echo "ERROR:"
-    echo "File '${DECATHLON_DIR}/dataset.json' is not there, aborting."
-    echo "Please download the Decathlon dataset, extract it, and point this script "
-    echo "to that directory."
-    exit
-fi
-
 clear
 echo "Script to train Decathlon Brain Tumor Segmentation (BraTS) U-Net model"
 echo "======================================================================"
@@ -64,6 +55,15 @@ echo "Then extract the data from the tar file"
 echo "tar -xvf Task01_BrainTumour.tar"
 echo "Make sure to change the DECATHLON_DIR variable in this script"
 echo " to wherever you untarred the dataset."
+
+if [[ ! -f ${DECATHLON_DIR}/dataset.json ]] ; then
+    echo " "
+    echo "ERROR:"
+    echo "File '${DECATHLON_DIR}/dataset.json' is not there, aborting."
+    echo "Please download the Decathlon dataset, extract it, and point this script "
+    echo "to that directory."
+    exit
+fi
 
 echo " "
 echo "*****************************************"
