3fe483965f0bdce87d3ce786b062a6a299b882b8
Tony Reina <g.anthony.reina@intel.com>
2019-01-16 14:47:39 -0800

    Moving Docker and updating bash script

--- single-node/openvino_saved_model/run_unet_demo_movidius.sh
+++ single-node/openvino_saved_model/run_unet_demo_movidius.sh
@@ -1,5 +1,32 @@
-#cd openvino_saved_model
-#python inference_openvino.py -l ${INTEL_CVSDK_DIR}/inference_engine/lib/ubuntu_16.04/intel64/libcpu_extension_avx2.so --plot --stats -d MYRIAD
-python inference_openvino.py -l ${INTEL_CVSDK_DIR}/inference_engine/lib/centos_7.4/intel64/libcpu_extension_avx2.so --plot --stats -d MYRIAD
+#!/bin/sh
+# ----------------------------------------------------------------------------
+# Copyright 2019 Intel
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 
+if [ -d "openvino_saved_model" ]; then
+  cd openvino_saved_model
+fi
 
+if [ -f /etc/redhat-release ]; then
+  # CentOS
+  OPENVINO_LIB=${INTEL_CVSDK_DIR}/inference_engine/lib/centos_7.4/intel64/
+fi
+
+if [ -f /etc/lsb-release ]; then
+  # Ubuntu
+  OPENVINO_LIB=${INTEL_CVSDK_DIR}/inference_engine/lib/ubuntu_16.04/intel64/
+fi
+
+python inference_openvino.py -l ${OPENVINO_LIB}/libcpu_extension_avx2.so \
+       --plot --stats -d MYRIAD
