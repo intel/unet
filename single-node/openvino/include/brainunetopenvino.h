e28466b1ae85248416cdf5d58fe0a4caeec008d3
Tony Reina <g.anthony.reina@intel.com>
2019-02-02 19:31:09 -0800

    Cleaning up C++

--- single-node/openvino/include/brainunetopenvino.h
+++ single-node/openvino/include/brainunetopenvino.h
@@ -1,39 +1,50 @@
 
-#include <gflags/gflags.h>
-#include <iostream>
+/* Copyright (c) 2019 Intel Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef OPENVINO_H
+#define OPENVINO_H
+
+#include "../src/cnpy/cnpy.h"
+#include <chrono>
 #include <fstream>
-#include <memory>
 #include <functional>
+#include <gflags/gflags.h>
+#include <iomanip>
+#include <iostream>
+#include <limits>
 #include <map>
+#include <memory>
 #include <random>
 #include <string>
-#include <vector>
 #include <time.h>
-#include <chrono>
-#include <limits>
-#include <iomanip>
-#include "../src/cnpy/cnpy.h"
-
+#include <vector>
 
 #include <inference_engine.hpp>
 #include <opencv2/opencv.hpp>
 
-
-
+#endif // OPENVINO_H
 
 #ifndef BRAINUNETOPENVINO_H
 #define BRAINUNETOPENVINO_H
 using namespace InferenceEngine;
 
-
-class BrainUnetOpenVino
-{
+class BrainUnetOpenVino {
 public:
-   double* loaded_data_masks;
-   double* loaded_data;
-   void loadNumpyData(cnpy::NpyArray &arr, cnpy::NpyArray &arr_msks);
-   void makeInference(int img_index, InferenceEngine::TargetDevice targetDevice, cnpy::NpyArray &arr, cnpy::NpyArray &arr_msks);
-
+  double *loaded_data_masks;
+  double *loaded_data;
+  void loadNumpyData(cnpy::NpyArray &arr, cnpy::NpyArray &arr_msks);
+  void makeInference(int img_index, InferenceEngine::TargetDevice targetDevice,
+                     cnpy::NpyArray &arr, cnpy::NpyArray &arr_msks);
 };
 
 #endif // BRAINUNETOPENVINO_H
--- single-node/openvino/src/main.cpp
+++ single-node/openvino/src/main.cpp
@@ -1,17 +1,16 @@
-#include <gflags/gflags.h>
-#include <iostream>
-#include <fstream>
-#include <memory>
-#include <functional>
-#include <map>
-#include <random>
-#include <string>
-#include <vector>
-#include <iomanip>
+/* Copyright (c) 2019 Intel Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
-#include <inference_engine.hpp>
 #include "../include/brainunetopenvino.h"
-
 
 using namespace InferenceEngine;
 using namespace std;
@@ -20,104 +19,92 @@ InferenceEngine::TargetDevice PLUGIN_NAME = TargetDevice::eCPU;
 int image_file_index;
 InferenceEngine::TargetDevice plugin_name;
 
-
-void printUsage(const string &app_name = string())
-{
-    cout << endl << "To run application in default configuration use:" << endl;
-    if (!app_name.empty())
-        cout << app_name << " ";
-    cout << "-p" << endl << endl;
-    cout << "Usage:" << endl;
-    if (!app_name.empty())
-        cout << app_name << endl;
-    cout << "     [-h] [--h] [-help] [--help] : display this usage page. No other commands accepted." << endl;
-    cout << "     [-p MYRIAD | myriad | CPU | cpu] : select plugin (defaults is CPU)" << endl;
-    cout << "     [-f filename] : specify the image number to be detected and recognized\n" << endl;
+void printUsage(const string &app_name = string()) {
+  cout << endl << "To run application in default configuration use:" << endl;
+  if (!app_name.empty())
+    cout << app_name << " ";
+  cout << "-p" << endl << endl;
+  cout << "Usage:" << endl;
+  if (!app_name.empty())
+    cout << app_name << endl;
+  cout << "     [-h] [--h] [-help] [--help] : display this usage page. No "
+          "other commands accepted."
+       << endl;
+  cout << "     [-p MYRIAD | myriad | CPU | cpu] : select plugin (defaults is "
+          "CPU)"
+       << endl;
+  cout << "     [-f filename] : specify the image number to be detected and "
+          "recognized\n"
+       << endl;
 }
-
-
 
 // Parses the command line to find the plugin to use.
 void parseArgs(int argc, char *argv[])
 
 {
 
-    try
+  try {
+    /*if (argc < 2)
     {
-        /*if (argc < 2)
-        {
-            printUsage(argv[0]);
-            throw "Insufficient number of command line arguments.";
-        } // end if
-         */
-        // Specify the default file name in case users don't pass it
-        image_file_index = IMAGE_FILE_INDEX;
-        plugin_name = PLUGIN_NAME;
+        printUsage(argv[0]);
+        throw "Insufficient number of command line arguments.";
+    } // end if
+     */
+    // Specify the default file name in case users don't pass it
+    image_file_index = IMAGE_FILE_INDEX;
+    plugin_name = PLUGIN_NAME;
 
-        // Start parsing out parameters
-        for (int i = 1; i < argc; i++)
-        {
-            string arg = argv[i];
+    // Start parsing out parameters
+    for (int i = 1; i < argc; i++) {
+      string arg = argv[i];
 
-            if (arg == "-h")	{ // users want to see help
-                printUsage(argv[0]);
-            }
+      if (arg == "-h") { // users want to see help
+        printUsage(argv[0]);
+      }
 
-            if (arg == "-f")
-                        {
-                            if (i + 1 < argc)
-                            {
-                                i++;
-                                image_file_index = std::stoi(argv[i]);
-                            }
-                            else
-                                throw i; // invalid parameter
-                        } // end if (-f)
+      if (arg == "-f") {
+        if (i + 1 < argc) {
+          i++;
+          image_file_index = std::stoi(argv[i]);
+        } else
+          throw i; // invalid parameter
+      }            // end if (-f)
 
-            if (arg == "-p")	{ // if users pass this option, mean they want test this particular interface
+      if (arg == "-p") { // if users pass this option, mean they want test this
+                         // particular interface
 
-                if (i + 1 < argc)
-                {
-                    i++;
-                    arg = argv[i];
-                    if (arg == "myriad" || arg == "MYRIAD")
-                       plugin_name = TargetDevice::eMYRIAD;
-                    if (arg == "cpu" || arg == "CPU")
-                       plugin_name = TargetDevice::eCPU;
-                }
-            }
+        if (i + 1 < argc) {
+          i++;
+          arg = argv[i];
+          if (arg == "myriad" || arg == "MYRIAD")
+            plugin_name = TargetDevice::eMYRIAD;
+          if (arg == "cpu" || arg == "CPU")
+            plugin_name = TargetDevice::eCPU;
         }
+      }
     }
-    catch (...)
-        {
-            cout << "Unexpected ERROR parsing command line." << endl;
-            cout << "Use parameter -help for a list of valid parameters." << endl;
+  } catch (...) {
+    cout << "Unexpected ERROR parsing command line." << endl;
+    cout << "Use parameter -help for a list of valid parameters." << endl;
 
-            throw "ERROR parsing command line.";
-        }
+    throw "ERROR parsing command line.";
+  }
 }
 
+int main(int argc, char *argv[]) {
+  std::cout << "Starting program" << std::endl;
+  BrainUnetOpenVino brainunetobj;
+  cnpy::NpyArray arr;
+  cnpy::NpyArray arr_msks;
 
+  try {
+    parseArgs(argc, argv); // request users for default
+    brainunetobj.loadNumpyData(arr, arr_msks);
+    brainunetobj.makeInference(image_file_index, plugin_name, arr, arr_msks);
 
-int main(int argc, char *argv[])
-{
-    std::cout<<"Starting program"<<std::endl;
-    BrainUnetOpenVino brainunetobj;
-    cnpy::NpyArray arr;
-    cnpy::NpyArray arr_msks;
+  } catch (...) {
+    cout << "Unspecified ERROR occurred during execution." << endl;
+  }
 
-    try
-       {
-           parseArgs(argc, argv);  // request users for default
-           brainunetobj.loadNumpyData(arr,arr_msks);
-           brainunetobj.makeInference(image_file_index,plugin_name,arr,arr_msks);
-
-      }
-    catch (...)
-               {
-                  cout << "Unspecified ERROR occurred during execution." << endl;
-               }
-
-    return 0;
-
- }
+  return 0;
+}
