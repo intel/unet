a0005dc3c341e94a064051956c4af7357cc890f7
Tony Reina <g.anthony.reina@intel.com>
2019-04-12 18:03:34 -0700

    144x144

--- 2D/settings.py
+++ 2D/settings.py
@@ -20,7 +20,7 @@
 import psutil  # pip install psutil
 import os
 
-DATA_PATH = os.path.join("../../data/decathlon/128x128/")
+DATA_PATH = os.path.join("../../data/decathlon/144x144/")
 DATA_FILENAME = "Task01_BrainTumour.h5"
 OUT_PATH = os.path.join("./output/")
 INFERENCE_FILENAME = "unet_model_for_decathlon.hdf5"
