86c2691c1bfebdc18c9de05975d56336b9742eb2
Tony Reina <g.anthony.reina@intel.com>
2020-12-27 11:44:46 -0800

    Cleaning up arguments

--- 2D/argparser.py
+++ 2D/argparser.py
@@ -94,7 +94,7 @@ parser.add_argument("--output_pngs",
                     help="the directory for the output prediction pngs")
 parser.add_argument("--input_filename",
                     help="Name of saved TensorFlow model directory",
-                    default=os.path..join(settings.OUT_PATH,settings.INFERENCE_FILENAME)
+                    default=os.path.join(settings.OUT_PATH,settings.INFERENCE_FILENAME)
 
 args = parser.parse_args()
 
