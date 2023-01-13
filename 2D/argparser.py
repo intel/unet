14b2df6c4f8f9b2905a6b34d7ab52c8c1c74db70
Aditya Sirvaiya <aditya.sirvaiya@intel.com>
2023-01-13 17:51:17 +0530

    Update argparser.py

--- 2D/argparser.py
+++ 2D/argparser.py
@@ -91,6 +91,14 @@ parser.add_argument("--output_pngs",
 parser.add_argument("--input_filename",
                     help="Name of saved TensorFlow model directory",
                     default=os.path.join(settings.OUT_PATH,settings.INFERENCE_FILENAME))
+parser.add_argument("--BF16", help="auto mixed precision",
+                    action="store_true")
+parser.add_argument("--OMP", help="openMP thread settings",
+                    action="store_true")
+parser.add_argument("--device_type", type=str,
+                    default=settings.device,
+                    help="CPU|GPU")
+
 
 args = parser.parse_args()
 
