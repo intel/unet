42c70782ea6f1494fd1a369dda2af1c7a4b26c03
Aditya Sirvaiya <aditya.sirvaiya@intel.com>
2023-01-20 13:18:41 +0530

    Update train.py

--- 2D/train.py
+++ 2D/train.py
@@ -58,6 +58,36 @@ def set_itex_amp(amp_target):
 
     print("Set itex for AMP (auto_mixed_precision, {}_FP32)".format(amp_target)
 
+def test_oneDNN():
+    import tensorflow as tf
+
+    import os
+
+    def get_mkl_enabled_flag():
+
+        mkl_enabled = False
+        major_version = int(tf.__version__.split(".")[0])
+        minor_version = int(tf.__version__.split(".")[1])
+        if major_version >= 2:
+            if minor_version < 5:
+                from tensorflow.python import _pywrap_util_port
+            elif minor_version >= 9:
+
+                from tensorflow.python.util import _pywrap_util_port
+                onednn_enabled = int(os.environ.get('TF_ENABLE_ONEDNN_OPTS', '1'))
+
+            else:
+                from tensorflow.python.util import _pywrap_util_port
+                onednn_enabled = int(os.environ.get('TF_ENABLE_ONEDNN_OPTS', '0'))
+            mkl_enabled = _pywrap_util_port.IsMklEnabled() or (onednn_enabled == 1)
+        else:
+            mkl_enabled = tf.pywrap_tensorflow.IsMklEnabled()
+        return mkl_enabled
+
+    print ("We are using Tensorflow version", tf.__version__)
+    print("oneDNN enabled :", get_mkl_enabled_flag())
+test_oneDNN()    
+          
 
 """
 For best CPU speed set the number of intra and inter threads
